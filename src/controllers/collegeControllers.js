66a3e7847e7682bdba89d28f8cce875501b1f0d9
Deepak <deepakgunpal0@gmail.com>
2022-05-08 14:56:34 +0530

    final code

--- src/controllers/collegeControllers.js
+++ src/controllers/collegeControllers.js
@@ -12,54 +12,54 @@ const createCollege = async function (req, res) {
   try {
     let data = req.body;
 
-    if (!isvalidRequestBody(data)) {
-      return res
-        .status(400)
-        .send({ status: false, message: "please provide College details" });
-    }
+        if (!isvalidRequestBody(data)) {
+            return res
+                .status(400)
+                .send({ status: false, message: "please provide College details" });
+        }
 
-    if (!req.body.name) {
-      return res
-        .status(400)
-        .send({ status: false, message: "name is required" });
-    }
-    if (!req.body.fullName) {
-      return res.status(400).send({
-        status: false,
-        message: `fullName is required as example - Indian Institute of Technology, Hyderabad `,
-      });
-    }
-    
-    if (!req.body.logoLink) {
-      return res
-        .status(400)
-        .send({ status: false, message: "logoLink is required" });
-    }
+        if (!req.body.name) {
+            return res
+                .status(400)
+                .send({ status: false, message: "name is required" });
+        }
+        if (!req.body.fullName) {
+            return res
+                .status(400)
+                .send({
+                    status: false,
+                    message: `fullName is required as example - Indian Institute of Technology, Hyderabad `,
+                });
+        }
+        if (!req.body.logoLink) {
+            return res
+                .status(400)
+                .send({ status: false, message: "logoLink is required" });
+        }
 
-    const validlogoLink =
-      /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/.test(
-        req.body.logoLink
-      );
-    if (!validlogoLink) {
-      return res.status(404).send({ status: false, message: `Invalid url` });
+        const validlogoLink = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/.test(req.body.logoLink)
+        if (!validlogoLink) {
+            return res
+                .status(400)
+                .send({ status: false, message: `Invalid url` });
+
+        }
+
+        let name = await collegeModel.findOne({ name: req.body.name });
+        console.log(name);
+        if (name) {
+            return res
+                .status(400)
+                .send({ status: false, message: `${req.body.name} already exist` });
+        }
+
+        let college = await collegeModel.create(data);
+
+        res.status(201).send({ status: true, data: college });
+    } catch (error) {
+        res.status(500).send({ status: false, msg: error.message });
     }
-
-    let name = await collegeModel.findOne({ name: req.body.name });
-    console.log(name);
-    if (name) {
-      return res
-        .status(400)
-        .send({ status: false, message: `${req.body.name} already exist` });
-    }
-
-    let college = await collegeModel.create(data);
-
-    res.status(201).send({ status: true, data: college });
-  } catch (error) {
-    res.status(500).send({ status: false, msg: error.message });
   }
-};
-
 //=========================================3 api ==================================================
 
 const collegeDetails = async function (req, res) {
@@ -106,9 +106,8 @@ const collegeDetails = async function (req, res) {
   } catch (error) {
     res.status(500).send({ status: false, msg: error.message });
   }
+
 };
 
 module.exports.createCollege = createCollege;
 module.exports.collegeDetails = collegeDetails;
-
-
